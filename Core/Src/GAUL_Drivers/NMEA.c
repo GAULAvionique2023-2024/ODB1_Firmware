c057f099b05577aa231384f6f0caf2df80561b02
SamLol12 <gagnonsamuel12@gmail.com>
2024-08-10 12:41:05 -0400

    Reapply "Merge pull request #14 from GAULAvionique2023-2024/L76LM33-hal"
    This reverts commit b5466534de6dbf1b98877a431b946ffef70d044b.

--- Core/Src/GAUL_Drivers/NMEA.c
+++ Core/Src/GAUL_Drivers/NMEA.c
@@ -1,118 +1,173 @@
 /*
  * NMEA.c
  *
- *  Created on: Mar 12, 2024
- *      Author: gagno
+ * Module to parse the time and the latitude/longitude from a RMC NMEA sentence.
+ *
+ *  Created on: May 12, 2024
+ *      Author: gagnon
+ *
+ *  Edited on: Jul 4, 2024
+ *      Autor: mathouqc
  */
 
 #include "GAUL_Drivers/NMEA.h"
 
-// Fonction pour convertir un float en tableau de uint8_t
-int32_t NMEA_FloatToBytes(float value) {
-    return (int32_t)value;
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Validate the NMEA sentence ID is RMC ($xxRMC).
+ * $GNRMC,080608.000,A,3029.461489,N,11430.072002,E,0.00,148.41,210423,,,D,V*09
+ *
+ * Takes 0.002ms to complete
+ *
+ * @param nmea_sentence: pointer to sentence array.
+ *
+ * @retval NMEA_OK RMC sentence
+ * @retval NMEA_ERROR Not a RMC sentence
+ */
+int8_t NMEA_ValidateRMC(const char *nmea_sentence) {
+    return strncmp(nmea_sentence+3, "RMC", 3) == 0 ? NMEA_OK : NMEA_ERROR;
 }
 
-// Fonction pour convertir une chaîne de caractères en float
-float NMEA_CharToFloat(char *data) {
-    return (float)atof(data);
-}
-
-// Fonction pour décoder une trame NMEA GPRMC
-uint8_t NMEA_Decode_GPRMC(const char *nmea_sentence, GPS_Data *gps_data) {
+/**
+ * Parse NMEA RMC sentence ($xxRMC).
+ * $GNRMC,080608.000,A,3029.461489,N,11430.072002,E,0.00,148.41,210423,,,D,V*09
+ *
+ * Latitude and longitude are 0.0 if there's no GPS fix.
+ * See GPS_Data struct for more details.
+ *
+ * Takes 0.13ms to complete
+ *
+ * @param nmea_sentence: pointer to sentence array.
+ * @param gps_data: pointer to structure to fill with parsed data.
+ *
+ * @retval 0 OK
+ * @retval NMEA_ERROR ERROR
+ */
+int8_t NMEA_ParseRMC(GPS_Data *gps_data, const char *nmea_sentence) {
     if (!nmea_sentence || !gps_data) {
-        return 0;
+        return NMEA_ERROR; // Error, NULL sentence or structure
     }
 
-    char *copy = strdup(nmea_sentence);
+    // Copy because strtok messes the string by replacing delimiter with \0
+    // and causes HardFault when called on literal string nmea_sentence
+    char *copy = strndup(nmea_sentence, NMEA_MAX_RMC_LENGTH);
     if (!copy) {
-        return 0;
+        return NMEA_ERROR; // Error while duplicating string
     }
 
+    int8_t tok_idx = 0; // Current field index
     char *token;
-    int field_index = 0;
-    int valid = 1;
 
-    // Valeurs par default
-    gps_data->latitude = DEFAULT_LATITUDE;
-    gps_data->latitude_indicator = DEFAULT_INDICATOR;
-    gps_data->longitude = DEFAULT_LONGITUDE;
-    gps_data->longitude_indicator = DEFAULT_INDICATOR;
-    gps_data->speed_knots = DEFAULT_SPEED;
-    gps_data->track_angle = DEFAULT_ANGLE;
-
-    // Extraire chaque champ de la trame
+    // Read first field
     token = strtok(copy, ",");
-    while (token != NULL && valid) {
-        switch (field_index) {
-        case 0:
-            if (strcmp(token, "$GPRMC") != 0) {
-                valid = 0;
+
+    // Only read lat/lon and avoid infinite loop
+    while (token != NULL && tok_idx < NMEA_MAX_TOKEN_TO_READ) {
+        if (tok_idx == 1) { // TIME
+            if (strnlen(token, 6) < 6) { // hhmmss.sss = 10 char, minimum is 6 char
+                return NMEA_ERROR; // Error with sentence
             }
-            break;
-        case 1:
-            printf("time: %s\r\n", token);
-            gps_data->time = NMEA_FloatToBytes(NMEA_CharToFloat(token));
-            break;
-        case 2:
-            if (strcmp(token, "A") != 0) {
-                valid = 0;
+
+            if (strnlen(token, 10) > 6 && token[6] != '.') { // Ensure dot position only if seconds are float
+                return NMEA_ERROR; // Error with sentence
             }
-            break;
-        case 3:
-            printf("latitude: %s\r\n", token);
-            gps_data->latitude = NMEA_FloatToBytes(NMEA_CharToFloat(token));
-            break;
-        case 4:
-            printf("latitude_indicator: %s\r\n", token);
-            gps_data->latitude_indicator = (uint8_t)token[0];
-            break;
-        case 5:
-            printf("longitude: %s\r\n", token);
-            gps_data->longitude = NMEA_FloatToBytes(NMEA_CharToFloat(token));
-            break;
-        case 6:
-            printf("longitude_indicator: %s\r\n", token);
-            gps_data->longitude_indicator = (uint8_t)token[0];
-            break;
-        case 7:
-            printf("speed: %s\r\n", token);
-            gps_data->speed_knots = NMEA_FloatToBytes(NMEA_CharToFloat(token));
-            break;
-        case 8:
-            printf("angle: %s\r\n", token);
-            gps_data->track_angle = NMEA_FloatToBytes(NMEA_CharToFloat(token));
-            break;
+
+            char time_raw[7]; // hhmmss\0
+            strncpy(time_raw, token, 6);
+            gps_data->time_raw = atoi(time_raw);
+
+            char hours[3] = "00"; // hh\0 = 3 char. Default value because strncpy doesn't add \0 at the end
+            strncpy(hours, token, 2);
+            gps_data->time.hours = atoi(hours);
+
+            char minutes[3] = "00"; // mm\0 = 3 char
+            strncpy(minutes, token+2, 2);
+            gps_data->time.minutes = atoi(minutes);
+
+            char seconds[7] = "00.000"; // ss.sss\0 = 7 char
+            strncpy(seconds, token+4, 6);
+            gps_data->time.seconds = atof(seconds);
+
+        } else if (tok_idx == 2) { // GPS FIX
+            // Ensure validity is A or V, else throw error
+            if (token[0] == 'A') {
+                gps_data->fix = 1; // GS Fix
+            } else if (token[0] == 'V') {
+                gps_data->fix = 0; // No GPD Fic
+            } else {
+                return NMEA_ERROR; // Error with sentence
+            }
+
+            if (gps_data->fix == 0) {
+                gps_data->latitude = 0;
+                gps_data->longitude = 0;
+                break; // No fix, so lat/lon are empty
+            }
+
+        } else if (tok_idx == 3) { // LATITUDE
+            // token = ddmm.mmmmmm (4 to 6 decimals)
+            if (strnlen(token, 9) < 9 || token[4] != '.') { //strlen au lieu de strnlen puisqu'on utilise déjà strtok, donc il y a assurément un \0 à la fin?
+                return NMEA_ERROR; // Error with sentence
+            }
+
+            char degrees[3] = "00"; // dd\0 = 3 char. Default value because strncpy doesn't add \0 at the end
+            strncpy(degrees, token, 2);
+
+            char minutes[10] = "00.000000"; // mm.mmmmmm\0 = 10 char max
+            strncpy(minutes, token+2, 9);
+
+            gps_data->latitude = atoi(degrees) + atof(minutes) / 60; // Degrees Minutes to Degrees Decimal conversion
+
+        } else if (tok_idx == 4) { // LATITUDE INDICATOR
+            int8_t sign;
+
+            // Ensure token is N or S, else throw error
+            if (token[0] == 'N') {
+                sign = 1;
+            } else if (token[0] == 'S') {
+                sign = -1;
+            } else {
+                return NMEA_ERROR; // Error with sentence
+            }
+
+            gps_data->latitude *= sign; // Change the sign of latitude depending on indicator
+
+        } else if (tok_idx == 5) { // LONGITUDE
+            // token = dddmm.mmmmmm (4 to 6 decimals)
+            if (strnlen(token, 10) < 10 || token[5] != '.') { //strlen au lieu de strnlen puisqu'on utilise déjà strtok, donc il y a assurément un \0 à la fin?
+                return NMEA_ERROR; // Error with sentence
+            }
+
+            char degrees[4] = "000"; // ddd\0 = 4 char. Default value because strncpy doesn't add \0 at the end
+            strncpy(degrees, token, 3);
+
+            char minutes[10] = "00.000000"; // mm.mmmmmm\0 = 10 char max
+            strncpy(minutes, token+3, 9);
+
+            gps_data->longitude = atoi(degrees) + atof(minutes) / 60; // Degrees Minutes to Degrees Decimal conversion
+
+        } else if (tok_idx == 6) { // LONGITUDE INDICATOR
+            int8_t sign;
+
+            // Ensure token is E or W, else throw error
+            if (token[0] == 'E') {
+                sign = 1;
+            } else if (token[0] == 'W') {
+                sign = -1;
+            } else {
+                return NMEA_ERROR; // Error with sentence
+            }
+
+            gps_data->longitude *= sign;
         }
+
         token = strtok(NULL, ",");
-        field_index++;
+        tok_idx++;
     }
 
     free(copy);
-    return valid;
-}
-
-// Fonction pour vérifier la validité d'une trame NMEA
-uint8_t NMEA_ValidTrame(const char *nmea_sentence) {
-    char *copy = strdup(nmea_sentence);
-    if (!copy) {
-        return 0;
-    }
-
-    uint8_t valid = 1;
-    char *token;
-
-    token = strtok(copy, ",");
-    if (token == NULL || strcmp(token, "$GPRMC") != 0) {
-        valid = 0;
-    } else {
-        token = strtok(NULL, ","); // Skip time
-        token = strtok(NULL, ","); // Validity indicator
-        if (token == NULL || strcmp(token, "A") != 0) {
-            valid = 0;
-        }
-    }
-
-    free(copy);
-    return valid;
+    return NMEA_OK;
 }
 
